6a6cce83e4683bbc37a9737ca92d385ad879a403
oxidmod <oxidmod@gmail.com>
2018-06-16 17:53:40 +0300

    A little bit random behaviour for Black Hole

--- src/Service/MessageGateway/BlackHoleGateway.php
+++ src/Service/MessageGateway/BlackHoleGateway.php
@@ -4,17 +4,21 @@ declare (strict_types=1);
 
 namespace Oxidmod\Messages\Service\MessageGateway;
 
+use Oxidmod\Messages\Service\MessageGateway\Exception\SendMessageException;
+
 /**
  * Does not send messages.
+ * Throw exception sometime =)
  */
 class BlackHoleGateway implements GatewayInterface
 {
     /**
-     * @param Message $message
-     *
-     * @return void
+     * {@inheritdoc}
      */
     public function send(Message $message): void
     {
+        if (\random_int(0, 3) === 0) {
+            throw new SendMessageException('Message was not sent.');
+        }
     }
 }
