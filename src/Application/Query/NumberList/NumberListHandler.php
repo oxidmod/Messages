a9f9d53c0e7b8e4a3ee7362b58a21f0e1e0807b1
oxidmod <oxidmod@gmail.com>
2018-06-16 19:40:22 +0300

    Query for numbers list

---
+++ src/Application/Query/NumberList/NumberListHandler.php
@@ -0,0 +1,41 @@
+<?php
+
+declare (strict_types=1);
+
+namespace Oxidmod\Messages\Application\Query\NumberList;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Oxidmod\Messages\Application\HandlerInterface;
+use Oxidmod\Messages\Entity\Number;
+
+/**
+ * Handler for NumberListQuery
+ */
+class NumberListHandler implements HandlerInterface
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    /**
+     * @param EntityManagerInterface $em
+     */
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    /**
+     * @param NumberListQuery $query
+     *
+     * @return array
+     */
+    public function handle(NumberListQuery $query): array
+    {
+        return $this->em->createQueryBuilder('n')
+            ->select(Number::class, 'n')
+            ->getQuery()
+            ->execute();
+    }
+}
---
+++ src/Application/Query/NumberList/NumberListQuery.php
@@ -0,0 +1,12 @@
+<?php
+
+declare (strict_types=1);
+
+namespace Oxidmod\Messages\Application\Query\NumberList;
+
+/**
+ *  Contains query params
+ */
+class NumberListQuery
+{
+}
